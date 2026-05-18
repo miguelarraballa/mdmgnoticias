4c68901b618762b5caf6b04e9916047f5aab2147
MDMG <info@mdmgdesarrolloweb.com>
2026-05-18 21:35:23 +0200

    feat (internationalization) v1.1.0 Added multilanguage to the app. Created en_GB and es_ES options

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('panel/', include('feeds.urls_admin')),
     path('', include('feeds.urls')),
 ]
